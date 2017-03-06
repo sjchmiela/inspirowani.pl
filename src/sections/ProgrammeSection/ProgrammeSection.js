1d5bfce696d1127d18127bb9ceb61ba19d5c95c8
Stanisław Chmiela <sjchmiela@gmail.com>
2017-03-06 09:13:25 +0100

    Programme Section finished

--- src/sections/ProgrammeSection/ProgrammeSection.js
+++ src/sections/ProgrammeSection/ProgrammeSection.js
@@ -3,18 +3,88 @@ import Section from '../../components/Section/Section';
 import ColumnsLayout from '../../components/ColumnsLayout/ColumnsLayout';
 import './ProgrammeSection.scss';
 
+const programme = [
+  {
+    header: '28 kwietnia, piątek',
+    appendix: 'lectures',
+    entries: [
+      { time: '14:00', name: 'początek zakwaterowania w miejscu noclegu dla osób z wykupionym pakietem K1' },
+      { time: '16:00', name: 'otwarcie recepcji Kongresu w Liceum Pijarów przy ul. Akacjowej 5' },
+      { time: '18:30', name: 'powitanie uczestników i otwarcie Kongresu' },
+      { time: '19:00', name: 'kolacja' },
+      { time: '20:00', name: 'Adoracja Najświętszego Sakramentu' },
+    ],
+    children: [
+      (<em>…a po niej czas na integrację (do ok. 23:00)</em>),
+    ]
+  },
+  {
+    header: '29 kwietnia, sobota',
+    appendix: 'workshops',
+    entries: [
+      { time: '8:15', name: 'jutrznia' },
+      { time: '8:45', name: 'śniadanie' },
+      { time: '9:30', name: 'konferencja I' },
+      { time: '10:30', name: 'konwersatorium I' },
+      { time: '11:45', name: 'Msza Święta w kościele pw. Najświętszego Imienia Maryi przy ul. Dzielskiego 1 pod przewodnictwem o. Pedro Aguado, Generała Zakonu Pijarów – mogą w niej brać udział również osoby nieuczestniczące w Kongresie' },
+      { time: '13:00', name: 'obiad' },
+      { time: '14:30', name: 'warsztaty (1. seria – z wyjątkiem warsztatu muzycznego)' },
+      { time: '16:15', name: 'konferencja II' },
+      { time: '17:15', name: 'konwersatorium II' },
+      { time: '18:15', name: 'kolacja' },
+      { time: '20:00', name: 'koncert zespołu Elendis' },
+    ]
+  },
+  {
+    header: '30 kwietnia, niedziela',
+    appendix: 'discussions',
+    entries: [
+      { time: '9:00', name: 'śniadanie' },
+      { time: '9:30', name: 'modlitwa poranna' },
+      { time: '10:00', name: 'konferencja III' },
+      { time: '11:00', name: 'konwersatorium III' },
+      { time: '12:00', name: 'warsztaty (2. seria)' },
+      { time: '13:30', name: 'obiad (do pobrania prowiant na kolację)' },
+      { time: '16:00', name: 'Msza Święta w kościele pw. Przemienienia Pańskiego przy ul. Pijarskiej 1' },
+      { time: '17:00', name: 'czas wolny (z propozycjami tras na spacer)' },
+    ]
+  },
+  {
+    header: '1 maja, poniedziałek',
+    appendix: 'project',
+    entries: [
+      { time: '8:30', name: 'śniadanie' },
+      { time: '9:30', name: 'Msza Święta w kościele pw. Matki Bożej Ostrobramskiej przy ul. Meissnera 20 (kaplica św. Józefa Kalasancjusza)' },
+      { time: '10:30', name: 'podsumowanie i zakończenie Kongresu w Liceum Pijarów, propozycje na przyszłość' },
+      { time: '12:00', name: 'obiad' },
+    ]
+  },
+]
+
 export default class Introduction extends Component {
   static propTypes = {
     zIndex: PropTypes.number,
   }
 
   getColumns() {
-    return [
-      this.renderLectures(),
-      this.renderWorkshops(),
-      this.renderOtherCards(),
-    ];
+    return programme.map(this.renderDay);
   }
+
+  renderDay = ({ header, entries, children, appendix }) => (
+    <div className={`ProgrammeSection-column ProgrammeSection-card ProgrammeSection-card--${appendix}`}>
+      <h3>{header}</h3>
+      <p>
+        {entries.map(this.renderEntry)}
+        {children}
+      </p>
+    </div>
+  )
+
+  renderEntry = ({ time, name }) => (
+    <div className="ProgrammeSection-entry">
+      <span className="ProgrammeSection-time">{time}</span>{name}
+    </div>
+  )
 
   renderLectures = () => (
     <div className="ProgrammeSection-column ProgrammeSection-card ProgrammeSection-card--lectures">
@@ -32,12 +102,12 @@ export default class Introduction extends Component {
       <h3>Warsztaty (dwa do wyboru)</h3>
       <ul>
         <li>Medytacja – modlitwa myślna</li>
-        <li>Modlitwa nieustająca w codzienności</li>
-        <li>Pismo Święte i Ojcowie Kościoła</li>
+        <li>Modlitwa nieustająca w codzienności</li>
+        <li>Pismo Święte i Ojcowie Kościoła</li>
         <li>Asceza – poznanie siebie</li>
-        <li>Cnoty kalasantyńskie – nieskończona cierpliwość i&nbsp;inne</li>
-        <li>Kalasancjusz w muzyce</li>
-        <li>Kalasancjusz w obrazach</li>
+        <li>Cnoty kalasantyńskie – nieskończona cierpliwość i inne</li>
+        <li>Kalasancjusz w muzyce</li>
+        <li>Kalasancjusz w obrazach</li>
       </ul>
     </div>
   )
@@ -51,19 +121,13 @@ export default class Introduction extends Component {
 
   renderDiscussions = () => (
     <div className="ProgrammeSection-card ProgrammeSection-card--discussions">
-      <p><strong>Konwersatoria</strong>, czyli czas na dzielenie się, dyskusje i&nbsp;wyjaśnienia</p>
+      <p><strong>Konwersatoria</strong>, czyli czas na dzielenie się, dyskusje i wyjaśnienia</p>
     </div>
   )
 
   renderProject = () => (
     <div className="ProgrammeSection-card ProgrammeSection-card--project">
-      <p><strong>Projekt Kongresowy</strong> – rozmowa ze&nbsp;Świętym</p>
-    </div>
-  )
-
-  renderDisclaimer = () => (
-    <div className="ProgrammeSection-card ProgrammeSection-card--disclaimer">
-      <p>Szczegółowy opis programu zostanie opublikowany w tym miejscu do końca 28&nbsp;lutego 2017.</p>
+      <p><strong>Projekt Kongresowy</strong> – rozmowa ze Świętym</p>
     </div>
   )
 
@@ -72,7 +136,6 @@ export default class Introduction extends Component {
       <Section className="ProgrammeSection" zIndex={this.props.zIndex}>
         <div className="ProgrammeSection-board">
           <h1 className="ProgrammeSection-header">Program</h1>
-          {this.renderDisclaimer()}
           <ColumnsLayout columns={this.getColumns()} />
         </div>
       </Section>
